0561dcff3b3f578661da8ecaeb56a03772814b64
carocamblor <ccamblor@udesa.edu.ar>
2022-03-19 15:09:10 -0300

    row one columns map

--- dashboard/src/components/RowOne/RowOne.js
+++ dashboard/src/components/RowOne/RowOne.js
@@ -2,13 +2,38 @@ import React from "react";
 import RowOneColumns from "../RowOneColumns/RowOneColumns";
 
 function RowOne() {
+
+    let rowOneColumns = [
+        {
+            title: "Products in Data Base",
+            data: "135",
+            color: "border-left-primary",
+            text: "text-primary",
+            icon: "fas fa-clipboard-list fa-2x text-gray-300"
+        },
+        {
+            title: "Amount in products",
+            data: "$546.456",
+            color: "border-left-success",
+            text: "text-success",
+            icon: "fas fa-dollar-sign fa-2x text-gray-300"
+        },
+        {
+            title: "Users quantity",
+            data: "38",
+            color: "border-left-warning",
+            text: "text-warning",
+            icon: "fas fa-user-check fa-2x text-gray-300"
+        },
+
+    ]
+
     return (
         <div className="row">
         
-        <RowOneColumns title="Products in Data Base" data="135" color="border-left-primary" text="text-primary" icon="fas fa-clipboard-list fa-2x text-gray-300"/>
-        <RowOneColumns title="Amount in products" data="$546.456" color="border-left-success" text="text-success" icon="fas fa-dollar-sign fa-2x text-gray-300"/>
-        <RowOneColumns title="Users quantity" data="38" color="border-left-warning" text="text-warning" icon="fas fa-user-check fa-2x text-gray-300"/>
-    </div> 
+        <RowOneColumns columns={rowOneColumns}/>
+
+        </div> 
     );
 };
 
--- dashboard/src/components/RowOneColumns/RowOneColumns.js
+++ dashboard/src/components/RowOneColumns/RowOneColumns.js
@@ -2,21 +2,24 @@ import React from "react";
 
 function RowOneColumns(props) {
     return (
-    <div className="col-md-4 mb-4">
-            <div className={`card ${props.color} shadow h-100 py-2`}> 
+    props.columns.map((column, idx) => 
+         <div className="col-md-4 mb-4" key={column + idx}>
+            <div className={`card ${column.color} shadow h-100 py-2`}> 
                 <div className="card-body">
                     <div className="row no-gutters align-items-center">
                         <div className="col mr-2">
-                            <div className={`text-xs font-weight-bold ${props.text} text-uppercase mb-1`}>{props.title}</div>
-                            <div className="h5 mb-0 font-weight-bold text-gray-800">{props.data}</div>
+                            <div className={`text-xs font-weight-bold ${column.text} text-uppercase mb-1`}>{column.title}</div>
+                            <div className="h5 mb-0 font-weight-bold text-gray-800">{column.data}</div>
                         </div>
                         <div className="col-auto">
-                            <i className={props.icon}></i>
+                            <i className={column.icon}></i>
                         </div>
                     </div>
                 </div>
             </div>
         </div>
+    )
+   
     );
 };
 
